ac410db4c9b11971b160d97351a39b597f164aed
mikereyes102 <mikereyes102@gmail.com>
2025-05-18 20:04:05 -0400

    Express server built. Backend connects to db and localhost successfully.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,27 @@
-
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
 
 // Connect to MongoDB
 connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Default Test Route
+app.get('/', (req, res) => {
+  res.send('Green Day Records API is up and running!');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
