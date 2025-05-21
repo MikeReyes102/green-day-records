40de01109678ee112c1c589753046e1c85a3e2b1
mikereyes102 <mikereyes102@gmail.com>
2025-05-21 16:23:02 -0400

    Added CRUD actions to add, update, delete, and fetch all products and add the routes. Also adding Sample Data for testing.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,5 @@
 const Product = require('../models/Product');
 
-// Add a new product
-// This functionality is reserved for admin users
-
-const addProduct = async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json(product);
-    } catch (error) {
-        res.status(400).json({ message: 'Error adding product', error });
-    }
-}; 
-
 // Get all products
 
 const getAllProducts = async (req, res) => {
@@ -37,4 +25,43 @@ const getProductById = async (req, res) => {
     }
 };
 
-module.exports = { addProduct, getAllProducts, getProductById };
+// The following functions are for Admin use only
+// Add, update, and delete product functions
+
+// Add a new product
+const addProduct = async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(400).json({ message: 'Error adding product', error });
+    }
+}; 
+
+// Update an existing product
+const updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Delete a product
+const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.json({ message: "Product deleted successfully" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports = { addProduct, getAllProducts, getProductById, updateProduct, deleteProduct };
